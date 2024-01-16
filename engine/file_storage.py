5e6a25da90217fcde1bb07883850278e9d6f0d6c
Wangutusi Arshton <wangarshton@gmail.com>
2024-01-17 02:41:45 +0300

    updated file_storage

--- engine/file_storage.py
+++ engine/file_storage.py
@@ -1,18 +1,7 @@
 #!/usr/bin/python3
 """Class Module for FileStorage"""
-from models.base_model import BaseModel
-import os
-import json
+from models.engine.file_storage import FileStorage
 
 
-class FileStorage():
-    """
-    A file storage class that saves and relodes to a JSON file and
-    """
-    
-    __file_path = "file.json"
-    __objects = {}
-    
-    def all(self):
-        """To return the __objects dictionary"""
-        return FileStorage.__objects
+storage = FileStorage()
+storage.reload()
