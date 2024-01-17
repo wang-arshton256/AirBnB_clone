1f1caec52e149f33ff9cfb4c9610309fdcd4d2f0
Wangutusi Arshton <wangarshton@gmail.com>
2024-01-17 04:40:09 +0300

    state

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""module that defines the State class."""
+from .base_model import BaseModel
+
+
+class State(BaseModel):
+    """A state class
+
+    Attr: name (str)
+    """
+
+    name = ""
