a43ec33555e93803de9ab932057014107a2cc165
Wangutusi Arshton <wangarshton@gmail.com>
2024-01-17 04:37:27 +0300

    review class

---
+++ models/review.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""module that defines the Review class."""
+from .base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ A review class
+
+    Attributes:
+        user_id (str):
+        place_id (str):
+        text (str):
+    """
+
+    user_id = ""
+    place_id = ""
+    text = ""
